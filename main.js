ab8f73fee96f57eff757cbfdd06314f22144cfef
cbott <cbott6@gmail.com>
2015-03-15 13:30:17 -0400

    working javascript

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+var prefixes = ["Robo", "Tech"];
+var suffixes = [" Eagles", "gineers"];
+
 $(function(){
 	$(".button").click(function(){
-		$("#team").html("name here")
+		
+		var pre_index = Math.floor(Math.random() * prefixes.length);
+		var pre = prefixes[pre_index];
+		
+		var suf_index = Math.floor(Math.random() * suffixes.length);
+		var suf = suffixes[suf_index];		
+		
+		var name = pre + suf;
+		$("#team").html(name)
 	});	
 });
