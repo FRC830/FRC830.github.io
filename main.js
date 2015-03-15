b74e0ac0bb067ac64d3f4bc7d7180f0cfa6a0f4d
cbott <cbott6@gmail.com>
2015-03-15 14:53:40 -0400

    more names

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var prefixes = ['Bionic', ' Cyber', ' Robo', ' Gear', ' STEM', ' STEAM', ' Killer', ' Techno', ' Tech', ' Mecha', ' Metal', ' Steel', ' Atomic', ' Atom', ' Viking', ' Mech', ' Rainbow', ' Sci', ' Laser', ' Panther', ' ', ' Green', ' Red', ' Blue', ' Flying', ' Titanium', ' Digital', ' Alpha', ' Electric'];
-var suffixes = [' Gears', ' Bots', 'gineers', ' Hawks', ' Horses', ' Cats', ' Pack', ' Team', ' Devils', ' Storm', ' Warriors', ' Knights', 'heads', ' Atoms', ' Robotics', ' Dogs', ' botics', ' Monkeys', ' Birds', ' Tech', ' Dragons', ' Ponies', ' Botz', ' Sharks', ' Nerds', ' Force', ' Fire', ' Machines', ' Tigers', ' Ninjas', ' Bulldogs', ' Titans', ' Rats', ' Panthers', ' Stallions', ' Beavers', ' Colts', ' Chaos', ' Wolves', ' Eagles'];
+var prefixes = [' Bionic', ' Cyber', ' Robo', ' Gear', ' STEM', ' STEAM', ' Killer', ' Techno', ' Tech', ' Mecha', ' Metal', ' Steel', ' Atomic', ' Atom', ' Viking', ' Mech', ' Rainbow', ' Sci', ' Laser', ' Panther', ' The', ' Green', ' Red', ' Blue', ' Flying', ' Titanium', ' Digital', ' Alpha', ' Electric', ' Phoenix', ' Electro', ' Thunder', ' Turbo', ' Lightning'];
+var suffixes = [' Gears', ' Bots', 'gineers', ' Hawks', ' Cats', ' Pack', ' Team', ' Devils', ' Storm', ' Warriors', ' Knights', 'heads', ' Atoms', ' Robotics', ' Dogs', 'botics', ' Monkeys', ' Birds', ' Tech', ' Dragons', ' Ponies', ' Botz', ' Sharks', ' Nerds', ' Force', ' Fire', ' Machines', ' Tigers', ' Ninjas', ' Bulldogs', ' Titans', ' Rats', ' Panthers', ' Stallions', ' Beavers', ' Colts', ' Chaos', ' Wolves', ' Eagles', ' Trojans', ' Spartans', ' Huskies', ' Penguins', 'trons'];
 
 $(function(){
 	$(".button").click(function(){
@@ -12,7 +12,7 @@ $(function(){
 
 		var number = Math.floor(Math.random() * (999)) + 6000;
 		
-		var name = "Team #" + number + " The " + pre + suf;
+		var name = "Team #" + number + pre + suf;
 		$("#team").html(name);
 	});	
 });
