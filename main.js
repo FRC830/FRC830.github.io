59daf5a39e9c369e1496b15d122bedc9bb49c2ec
cbott <cbott6@gmail.com>
2015-03-15 14:05:04 -0400

    more names

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var prefixes = ['Bionic', 'Cyber', 'Robo', 'Gear', 'STEM', 'STEAM', 'Killer', 'Techno', 'Tech', 'Mecha', 'Metal', 'Steel', 'Atomic', 'Atom', 'Viking', 'Mech', 'Rainbow', 'Sci', 'Laser', 'Panther', '', 'Green', 'Red', 'Blue', 'Flying', 'Titanium', 'Digital', 'Alpha'];
-var suffixes = ['Gears', 'Bots', 'gineers', 'Hawks', 'Horses', 'Cats', 'Pack', 'Team', 'Devils', 'Storm', 'Warriors', 'Knights', 'heads', 'Atoms', 'Robotics', 'Dogs', 'botics', 'Monkeys', 'Birds', 'Tech', 'Dragons', 'Ponies', 'Botz', 'Sharks', 'Nerds', 'Force', 'Fire', 'Machines', 'Tigers', 'Ninjas', 'Bulldogs', 'Titans', 'Rats', 'Panthers'];
+var prefixes = ['Bionic', ' Cyber', ' Robo', ' Gear', ' STEM', ' STEAM', ' Killer', ' Techno', ' Tech', ' Mecha', ' Metal', ' Steel', ' Atomic', ' Atom', ' Viking', ' Mech', ' Rainbow', ' Sci', ' Laser', ' Panther', ' ', ' Green', ' Red', ' Blue', ' Flying', ' Titanium', ' Digital', ' Alpha', ' Electric'];
+var suffixes = [' Gears', ' Bots', 'gineers', ' Hawks', ' Horses', ' Cats', ' Pack', ' Team', ' Devils', ' Storm', ' Warriors', ' Knights', 'heads', ' Atoms', ' Robotics', ' Dogs', ' botics', ' Monkeys', ' Birds', ' Tech', ' Dragons', ' Ponies', ' Botz', ' Sharks', ' Nerds', ' Force', ' Fire', ' Machines', ' Tigers', ' Ninjas', ' Bulldogs', ' Titans', ' Rats', ' Panthers', ' Stallions', ' Beavers', ' Colts', ' Chaos', ' Wolves', ' Eagles'];
 
 $(function(){
 	$(".button").click(function(){
