1220ff142fc31aad6b37eab688dddf25ac31a87d
cbott <cbott6@gmail.com>
2016-11-15 21:50:05 -0500

    update so we don't cover existing teams

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-var prefixes = [' Bionic', ' Cyber', ' Robo', ' Gear', ' STEM', ' STEAM', ' Killer', ' Techno', ' Tech', ' Mecha', ' Metal', ' Steel', ' Atomic', ' Atom', ' Viking', ' Mech', ' Rainbow', ' Sci', ' Laser', ' Panther', ' The', ' Green', ' Red', ' Blue', ' Flying', ' Titanium', ' Digital', ' Alpha', ' Electric', ' Electro', ' Thunder', ' Turbo', ' Lightning', ' Robotic', ' Iron', ' Plasma', ' Rocket'];
-var suffixes = [' Gears', ' Bots', 'gineers', ' Hawks', ' Cats', ' Pack', ' Team', ' Devils', ' Storm', ' Warriors', ' Knights', 'heads', ' Atoms', ' Robotics', ' Dogs', 'botics', ' Monkeys', ' Birds', ' Tech', ' Dragons', ' Ponies', ' Botz', ' Sharks', ' Nerds', ' Force', ' Fire', ' Machines', ' Tigers', ' Ninjas', ' Bulldogs', ' Titans', ' Rats', ' Panthers', ' Stallions', ' Beavers', ' Colts', ' Chaos', ' Wolves', ' Eagles', ' Trojans', ' Spartans', ' Huskies', ' Penguins', 'trons', ' Bees', ' Pirates', ' Mayhem', ' Martians', ' Phoenix', ' Rangers'];
+var prefixes = [' Bionic', ' Cyber', ' Robo', ' Gear', ' STEM', ' STEAM',
+				' Killer', ' Techno', ' Tech', ' Mecha', ' Metal', ' Steel', 
+				' Atomic', ' Atom', ' Viking', ' Mech', ' Rainbow', ' Sci', 
+				' Laser', ' Panther', ' The', ' Green', ' Red', ' Blue', 
+				' Flying', ' Titanium', ' Digital', ' Alpha', ' Electric', 
+				' Electro', ' Thunder', ' Turbo', ' Lightning', ' Robotic', 
+				' Iron', ' Plasma', ' Rocket'];
+				
+var suffixes = [' Gears', ' Bots', 'gineers', ' Hawks', ' Cats', ' Pack', 
+				' Team', ' Devils', ' Storm', ' Warriors', ' Knights', 'heads',
+				' Atoms', ' Robotics', ' Dogs', 'botics', ' Monkeys', ' Birds',
+				' Tech', ' Dragons', ' Ponies', ' Botz', ' Sharks', ' Nerds',
+				' Force', ' Fire', ' Machines', ' Tigers', ' Ninjas',
+				' Bulldogs', ' Titans', ' Rats', ' Panthers', ' Stallions',
+				' Beavers', ' Colts', ' Chaos', ' Wolves', ' Eagles',
+				' Trojans', ' Spartans', ' Huskies', ' Penguins', 'trons',
+				' Bees', ' Pirates', ' Mayhem', ' Martians', ' Phoenix', 
+				' Rangers'];
 
 $(function(){
 	$(".button").click(function(){
@@ -10,7 +26,7 @@ $(function(){
 		var suf_index = Math.floor(Math.random() * suffixes.length);
 		var suf = suffixes[suf_index];	
 
-		var number = Math.floor(Math.random() * (999)) + 6000;
+		var number = Math.floor(Math.random() * (999)) + 7000;
 		
 		var name = "Team #" + number + pre + suf;
 		$("#team").html(name);
